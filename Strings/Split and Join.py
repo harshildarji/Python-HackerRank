195e52bd3ff512ac978fa20afc88e7ab9e84cab7
Harshil <darjiharshil2994@gmail.com>
2017-12-14 11:15:04 +0530

    Strings 2-3

---
+++ Strings/Split and Join.py
@@ -0,0 +1,5 @@
+# String Split and Join
+# https://www.hackerrank.com/challenges/python-string-split-and-join/problem
+
+s = input().strip().split(" ")
+print("-".join(s))
---
+++ Strings/What's Your Name.py
@@ -0,0 +1,6 @@
+# What's Your Name?
+# https://www.hackerrank.com/challenges/whats-your-name/problem
+
+name = input().strip()
+surname = input().strip()
+print("Hello %s %s! You just delved into python." % (name, surname))
