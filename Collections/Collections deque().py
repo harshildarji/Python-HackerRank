a235f45fb6b15ca513b7050f7180146a4955480e
Harshil <darjiharshil2994@gmail.com>
2018-01-05 14:52:28 +0530

    Collections 6

--- Collections/Collections deque().py
+++ Collections/Collections deque().py
@@ -1,2 +1,12 @@
 # Collections.deque()
 # https://www.hackerrank.com/challenges/py-collections-deque/problem
+
+from collections import deque
+
+n = int(input().strip())
+d = deque()
+for _ in range(n):
+    line = input().split()
+    if len(line) > 1: exec('d.' + line[0] + "('" + line[1] + "')")
+    else: exec('d.' + line[0] + '()')
+print(*d)
