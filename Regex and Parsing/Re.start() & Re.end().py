6122093d6aab4c4d33dc2def5794653ba9a38fcb
Harshil <darjiharshil2994@gmail.com>
2018-02-15 15:37:44 +0530

    Regex and Parsing 5

---
+++ Regex and Parsing/Re.start() & Re.end().py
@@ -0,0 +1,6 @@
+# Re.start() & Re.end()
+# https://www.hackerrank.com/challenges/re-start-re-end/problem
+
+import re
+s, a = [input() for i in '12']
+[print('(%s, %s)' % (m.start(), m.end())) for m in re.finditer(a[:1] + '(?=' + a[1:] + ')', s)] or print('(-1, -1)')
